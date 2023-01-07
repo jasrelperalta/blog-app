20a871f7d3202771d579259f1c22c297b3826789
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2023-01-07 11:13:17 +0000

    fix:  comments to be updated when updating blog

--- src/services/blogs/update-blog.js
+++ src/services/blogs/update-blog.js
@@ -3,11 +3,12 @@ import { getDB, saveDB } from '../../utils/db/index.js';
 export const updateBlog = async (request, reply) => {
   const { params, body } = request;
   const { blogId: id } = params;
-  const { title, desc } = body;
+  const { title, desc, comments } = body;
   const db = await getDB();
 
   db.blogs[id].title = title || db.blogs[id].title;
   db.blogs[id].desc = desc || db.blogs[id].desc;
+  db.blogs[id].comments = comments || db.blogs[id].comments;
   db.blogs[id].updatedDate = new Date().getTime();
 
   await saveDB(db);
