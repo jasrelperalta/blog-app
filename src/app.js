d9f78a05f70e83d6582702ba9e0c6bcadb162d7a
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2023-01-08 15:02:10 +0000

    feat: add openAPI glue and swagger

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
-import { createBlog } from './services/blogs/create-blog.js';
-import { deleteBlog } from './services/blogs/delete-blog.js';
-import { getBlog } from './services/blogs/get-blog.js';
-import { getManyBlog } from './services/blogs/get-many-blog.js';
-import { updateBlog } from './services/blogs/update-blog.js';
-import { general } from './services/general/index.js';
-import { createComment } from './services/comments/create-comment.js';
-import { getManyComment } from './services/comments/get-many-comment.js';
-import { updateComment } from './services/comments/update-comment.js';
-import { getComment } from './services/comments/get-comment.js';
-import { deleteComment } from './services/comments/delete-comment.js';
+import openAPIGlue from 'fastify-openapi-glue';
+import swagger from '@fastify/swagger';
 
 const prefix = '/api';
 
@@ -19,30 +10,43 @@ export async function build () {
   const fastify = Fastify({ logger: true });
   fastify.register(sensible);
 
-  // returns { success : true }from /general/index.js
-  fastify.get(prefix, general);
+  const openAPIGlueOptions = {
+    prefix
+  };
 
-  // create blog
-  fastify.post(`${prefix}/blog`, createBlog);
-  // get many blogs
-  fastify.get(`${prefix}/blog`, getManyBlog);
-  // get a blog
-  fastify.get(`${prefix}/blog/:blogId`, getBlog);
-  // update a blog
-  fastify.put(`${prefix}/blog/:blogId`, updateBlog);
-  // delete a blog
-  fastify.delete(`${prefix}/blog/:blogId`, deleteBlog);
+  const swaggerOptions = {
+    exposeRoute: true
+  };
 
-  // create comment
-  fastify.post(`${prefix}/blog/:blogId/comment`, createComment);
-  // get comments of a blog
-  fastify.get(`${prefix}/blog/:blogId/comment`, getManyComment);
-  // get a comment of a blog
-  fastify.get(`${prefix}/blog/:blogId/comment/:commentId`, getComment);
-  // get comments of a blog
-  fastify.put(`${prefix}/blog/:blogId/comment/:commentId`, updateComment);
-  // delete a comment of a blog
-  fastify.delete(`${prefix}/blog/:blogId/comment/:commentId`, deleteComment);
+
+  fastify.register(swagger, swaggerOptions);
+  fastify.register(openAPIGlue, openAPIGlueOptions);
+
+  
+  // // returns { success : true }from /general/index.js
+  // fastify.get(prefix, general);
+
+  // // create blog
+  // fastify.post(`${prefix}/blog`, createBlog);
+  // // get many blogs
+  // fastify.get(`${prefix}/blog`, getManyBlog);
+  // // get a blog
+  // fastify.get(`${prefix}/blog/:blogId`, getBlog);
+  // // update a blog
+  // fastify.put(`${prefix}/blog/:blogId`, updateBlog);
+  // // delete a blog
+  // fastify.delete(`${prefix}/blog/:blogId`, deleteBlog);
+
+  // // create comment
+  // fastify.post(`${prefix}/blog/:blogId/comment`, createComment);
+  // // get comments of a blog
+  // fastify.get(`${prefix}/blog/:blogId/comment`, getManyComment);
+  // // get a comment of a blog
+  // fastify.get(`${prefix}/blog/:blogId/comment/:commentId`, getComment);
+  // // get comments of a blog
+  // fastify.put(`${prefix}/blog/:blogId/comment/:commentId`, updateComment);
+  // // delete a comment of a blog
+  // fastify.delete(`${prefix}/blog/:blogId/comment/:commentId`, deleteComment);
 
   return fastify;
 }
