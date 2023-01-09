747ef892ecc247e58b93f57396c25200e4d0f8af
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2023-01-09 22:34:05 +0000

    feat: added operations of comments

---
+++ src/specification/paths/comment/index.js
@@ -0,0 +1,136 @@
+export const comment = {
+  '/blog/:blogId/comment': {
+    parameters: [
+      {
+        $ref: '#/components/parameters/BlogParameterId'
+      }
+    ],
+    post: {
+      summary: 'Create a comment',
+      operationId: 'createComment',
+      parameters: [
+        {
+          $ref: '#/components/parameters/BlogParameterId'
+        }
+      ],
+      requestBody: {
+        description: 'The request body for the comment',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/CommentRequestRequiredObject'
+            }
+          }
+        },
+        required: true
+      },
+      responses: {
+        200: {
+          description: 'A comment',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/CommentObject'
+              }
+            }
+          }
+        }
+      }
+    },
+    get: {
+      summary: 'Get many comments of a blog',
+      operationId: 'getManyComment',
+      parameters: [
+        {
+          $ref: '#/components/parameters/BlogParameterId'
+        }
+      ],
+      responses: {
+        200: {
+          description: 'A list of comments of a blog',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/GetManyCommentResponseObject'
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  '/blog/:blogId/comment/:commentId': {
+    parameters: [
+      {
+        $ref: '#/components/parameters/BlogParameterId'
+      },
+      {
+        $ref: '#/components/parameters/CommentParameterId'
+      }
+    ],
+    get: {
+      summary: 'Get a comment of a blog',
+      operationId: 'getComment',
+      responses: {
+        200: {
+          description: 'A comment',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/CommentObject'
+              }
+            }
+          }
+        }
+      }
+    },
+    put: {
+      summary: 'Update a comment of a blog',
+      operationId: 'updateComment',
+      requestBody: {
+        description: 'The request body for the comment',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/CommentRequestRequiredObject'
+            }
+          }
+        },
+        required: true
+      },
+      responses: {
+        200: {
+          description: 'A comment',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/CommentObject'
+              }
+            }
+          }
+        }
+      }
+    },
+    delete: {
+      summary: 'Delete a comment of a blog',
+      operationId: 'deleteComment',
+      responses: {
+        200: {
+          description: 'successful response',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  success: {
+                    type: 'boolean'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
