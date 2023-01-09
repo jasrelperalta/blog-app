698281077b03b41d91ce9a3823856d83315e2ebb
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2023-01-09 22:33:44 +0000

    feat: added comment to paths

--- src/specification/paths/index.js
+++ src/specification/paths/index.js
@@ -1,7 +1,9 @@
 import { general } from './general/index.js';
 import { blog } from './blog/index.js';
+import { comment } from './comment/index.js';
 
 export const paths = {
   ...general,
-  ...blog
+  ...blog,
+  ...comment
 };
