c106320a70b67362085730679ef8b2671627b80a
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2023-01-09 22:32:44 +0000

    feat: added comment and limit parameters

--- src/specification/compoonents/parameters/index.js
+++ src/specification/compoonents/parameters/index.js
@@ -7,5 +7,23 @@ export const parameters = {
     schema: {
       type: 'string'
     }
+  },
+  CommentParameterId: {
+    name: 'commentId',
+    in: 'path',
+    required: true,
+    description: 'Id of comment on a blog post',
+    schema: {
+      type: 'string'
+    }
+  },
+  GetBlogLimit: {
+    name: 'limit',
+    in: 'query',
+    description: 'Number of items to be returned',
+    schema: {
+      type: 'number',
+      default: 5
+    }
   }
 };
